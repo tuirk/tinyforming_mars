06eb897f039c6a90e4c41e2b4ab4827787875dc0
Tuana <tuanairkey@gmail.com>
2025-09-20 08:50:06 +0000

    SideA Slot2 Projects for Cards 5, 6, and 7 (matching their Slot1 counter

--- src/lib/game/constants.ts
+++ src/lib/game/constants.ts
@@ -309,6 +309,16 @@ PROJECT_CARDS[4].sideA.slot1 = {
     effectType: "water",
     automaticTags: { ...emptyReq, Production: 1, Nature: 1 },
 };
+PROJECT_CARDS[4].sideA.slot2 = {
+    id: "5-A2",
+    name: "Lava Flows",
+    cost: { credits: 2, reducible: false },
+    tagRequirements: { ...emptyReq, Energy: 2, Nature: 1 },
+    parameterRequirements: { ...emptyParams },
+    effect: "Place 1 Heat Cube in a hex not reserved for water",
+    effectType: "heat",
+    automaticTags: { ...emptyReq, Production: 1, Science: 1 },
+};
 PROJECT_CARDS[4].sideB.slot1 = {
     id: "5-B1",
     name: "Great Dam",
@@ -342,6 +352,17 @@ PROJECT_CARDS[5].sideA.slot1 = {
     effectType: "water",
     automaticTags: { ...emptyReq, Nature: 2 },
 };
+PROJECT_CARDS[5].sideA.slot2 = {
+    id: "6-A2",
+    name: "Solar Power",
+    cost: { credits: 3, reducible: true },
+    tagRequirements: { ...emptyReq, Production: 1, Science: 1 },
+    parameterRequirements: { ...emptyParams },
+    costReductionRule: "Reduce the cost by 1 Credit for each city on the Center row",
+    effect: "Gain 1 Heat Cube",
+    effectType: "heat",
+    automaticTags: { ...emptyReq, Energy: 1, Nature: 1 },
+};
 PROJECT_CARDS[5].sideB.slot1 = {
     id: "6-B1",
     name: "Nuclear Power",
@@ -374,6 +395,16 @@ PROJECT_CARDS[6].sideA.slot1 = {
     effect: "Place 1 Water Cube. It must be placed in any unoccupied hex on the South row.",
     effectType: "water",
     automaticTags: { ...emptyReq, Production: 1, Nature: 1 },
+};
+PROJECT_CARDS[6].sideA.slot2 = {
+    id: "7-A2",
+    name: "Power Grid",
+    cost: { credits: 5, reducible: false },
+    tagRequirements: { ...emptyReq, Energy: 1, Production: 1 },
+    parameterRequirements: { ...emptyParams },
+    effect: "Gain 1 Heat Cube. Gain 1 Credit for each city",
+    effectType: "heat",
+    automaticTags: { ...emptyReq, Nature: 1, Science: 1 },
 };
 PROJECT_CARDS[6].sideB.slot1 = {
     id: "7-B1",
@@ -524,3 +555,6 @@ export const getInitialGameState = (playerMapId: MapId, aiMapId: MapId): GameSta
     
 
 
+
+
+    
